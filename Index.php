17581b73840f68871c2b7560f5bfd01e83495123
akhira.firdaza@yahoo.com <akhira.firdaza@yahoo.com>
2019-10-15 22:04:17 +0700

    mengedit file index.php

--- Index.php
+++ Index.php
@@ -1,6 +1,6 @@
 <?php
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
-echo "<h3>Akhira Maulidio Firdaza (175410109)</h3>"
+echo "<h3>Akhira Maulidio F (175410109)</h3>"
 
 ?>
