abef07dfc4b8fcf73daa08d2782535a8567a20ca
farizasandaira98 <muhfariza98@gmail.com>
2019-10-15 15:25:25 +0700

    Saya ngubah yo

--- Index.php
+++ Index.php
@@ -2,9 +2,8 @@
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
 
+echo "<h3>Muh Fariza (175410087)</h3>"
 echo "<h3>Akhira Maulidio F (175410109)</h3>"
-
-
 echo "<h3>Didik darmawan(175410104)</h3>"
 
 
